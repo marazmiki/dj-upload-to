d436ba42a6f58584060bd4717ec3b7004d9e0bdd
Mikhail Porokhovnichenko <marazmiki@gmail.com>
2017-04-17 23:17:45 +0700

    Removed user_args from py.test runner

--- setup.py
+++ setup.py
@@ -16,10 +16,6 @@ DIR = os.path.dirname(__file__)
 
 
 class pytest(setuptools.command.test.test):
-    user_options = [
-        ('pytest-args=', 'a', 'Arguments to pass to py.test')
-    ]
-
     def initialize_options(self):
         setuptools.command.test.test.initialize_options(self)
         self.pytest_args = ['tests.py']
